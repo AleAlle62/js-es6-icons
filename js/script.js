2e79f0b8fb57b502b87bdf2c8b33aab32f25a9ed
AleAlle62 <basketallegrini@gmail.com>
2022-11-09 18:40:27 +0100

    miss m3

--- js/script.js
+++ js/script.js
@@ -132,3 +132,8 @@ for (let i = 0; i < arrayOggetti.length; i++) {
 
 //MILESTONE 3 'Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.'
 
+
+
+// funzione per cui SE viene selezionato UN OPZIONE, allora le altre classi diventano invisibili e viceversa
+
+
